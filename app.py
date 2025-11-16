2d0b24123f629fa6a0d3c8ce14091bc2a6ab02e3
Creator-CSE267 <capstoneproject436@gmail.com>
2025-11-16 10:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import A4
 from login import login_page, init_user_db
 from password_reset import password_reset
+from login import login_page, init_user_db, password_reset
+
 
 
 
